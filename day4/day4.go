18b7850618cec20e15be9a64c6d8dd7ed4bfa2fd
tiago fernandes <tiago.fernandes@bitmaker-software.com>
2020-01-15 10:46:55 +0000

    day4, part 2

--- day4/day4.go
+++ day4/day4.go
@@ -32,7 +32,45 @@ func Day4(start, end int) (count int) {
 	return
 }
 
+func Day4Part2(start, end int) (count int) {
+	for ; start <= end; start++ {
+		str := strconv.Itoa(start)
+		adjacent := false
+		groupCount := 0
+		increasing := true
+		prevS := ""
+		prevI := -1
+		for _, s := range str {
+			curS := string(s)
+			curI, _ := strconv.Atoi(curS)
+			if prevS == curS {
+				groupCount++
+			} else {
+				if groupCount == 1 {
+					adjacent = true
+				}
+				groupCount = 0
+			}
+			if curI < prevI {
+				increasing = false
+				break
+			}
+			prevS = curS
+			prevI = curI
+		}
+		if groupCount == 1 {
+			adjacent = true
+		}
+		if adjacent && increasing {
+			count++
+		}
+	}
+	return
+}
+
 func main() {
 	count := Day4(168630, 718098)
+	fmt.Println(count)
+	count = Day4Part2(168630, 718098)
 	fmt.Println(count)
 }
